a46d7135e5a7dc0a0d00fcc345c3235aa49c82fa
Simon Flohr <sflohr2@wgu.edu>
2024-12-31 01:18:45 -0600

    Initiate frontend and backend frameworks
    - Initiate Angular as the frontend.
    - Initiate Spring Boot as the backend.

---
+++ backend/src/main/java/com/bondspace/BondSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.bondspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondSpaceApplication.class, args);
+	}
+
+}
