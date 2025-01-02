5713205234e0f9ac7f7fbbbfbce7eeb94ab07d07
Simon Flohr <sflohr2@wgu.edu>
2025-01-01 23:47:08 -0600

    Create a seperate primary key column for 'user_space' table
    - Also change the data types for 'space_notifications' in 'spaces' and 'notifications' in 'users' from JSONB[] to INT[].

--- backend/src/main/resources/db/sql/schema.sql
+++ backend/src/main/resources/db/sql/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
     user_bio TEXT,
     user_picture VARCHAR(255),
     spaces INT[],
-    notifications JSONB[]
+    notifications INT[]
 );
 
 -- Create the 'spaces' table
@@ -23,16 +23,16 @@ CREATE TABLE IF NOT EXISTS spaces (
     space_picture VARCHAR(255),
     memories INT[],
     users INT[] NOT NULL,
-    space_notifications JSONB[]
+    space_notifications INT[]
 );
 
 -- Create the 'user_space' join table
 CREATE TABLE IF NOT EXISTS user_space (
+    id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     space_id INT NOT NULL,
     user_role VARCHAR(50) NOT NULL,
     user_joined_at TIMESTAMP,
-    PRIMARY KEY (user_id, space_id),
     CONSTRAINT fk_user_space_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     CONSTRAINT fk_user_space_space FOREIGN KEY (space_id) REFERENCES spaces (id) ON DELETE CASCADE
 );
