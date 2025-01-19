100ffca7d5790651f415d3e01c9a3937914a3156
Simon Flohr <sflohr2@wgu.edu>
2025-01-19 12:59:32 -0600

    chore(repository): update UserRepository interface
    - Add or modify methods for checking user existence by email address.
    - Ensure support for retrieving users by email address with optional handling.

--- backend/src/main/java/com/bondspace/repository/UserRepository.java
+++ backend/src/main/java/com/bondspace/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.bondspace.repository;
 import com.bondspace.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByEmailAddress(String emailAddress);
+    Optional<User> findByEmailAddress(String emailAddress);
 }
