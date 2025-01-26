a8dbdef2aaacb4c6cbea008557616d9d0bbc10a6
Simon Flohr <sflohr2@wgu.edu>
2025-01-25 18:57:21 -0600

    feat(frontend): create login component and update routes
    - Create `LoginComponent` with a responsive form for user login.
    - Design `login.component.html` with validation and links to the registration page.
    - Update `app.routes.ts` to include a route for the login page.
    - Integrate the login component into the application's routing flow.

---
+++ frontend/angular-bondspace-web-app/src/app/component/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onSubmit() {
+    if (this.email && this.password) {
+      this.authService.login(this.email, this.password).subscribe({
+        next: () => {
+          this.router.navigate(['/dashboard']);
+        },
+        error: (error) => {
+          console.error('Login failed:', error);
+        }
+      });
+    }
+  }
+
+}
