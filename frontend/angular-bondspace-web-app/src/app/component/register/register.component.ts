e4e2c83542ecaf7bfc83915bfadd1fddc1f5639b
Simon Flohr <sflohr2@wgu.edu>
2025-01-26 16:45:15 -0600

    feat(frontend): implement complete register component
    - Created `register.component.html` with a fully responsive registration form, including fields for email, password, password confirmation, first name, and last name.
    - Added form validation logic for all fields to ensure proper user input, including checks for required fields and password confirmation matching.
    - Updated `register.component.ts` beyond boilerplate to support form submission and validation.
    
    These changes add a fully functional registration page, laying the groundwork for user account creation with robust validation.

--- frontend/angular-bondspace-web-app/src/app/component/register/register.component.ts
+++ frontend/angular-bondspace-web-app/src/app/component/register/register.component.ts
@@ -1,12 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  email: string = '';
+  password: string = '';
+  passwordConfirm: string = '';
+  firstName = '';
+  lastName = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onSubmit() {
+    if (this.email
+      && this.password
+      && this.passwordConfirm
+      && this.firstName
+      && this.lastName
+    ) {
+      this.authService.register(
+        this.email,
+        this.password,
+        this.firstName,
+        this.lastName
+      ).subscribe({
+        next: (response) => {
+          console.log('Registration response:', response);
+          this.router.navigate(['/login']);
+        },
+        error: (error) => {
+          console.log('Registration failed:', error);
+          alert(error.error || 'Registration failed')
+        }
+      });
+    }
+  }
 
 }
