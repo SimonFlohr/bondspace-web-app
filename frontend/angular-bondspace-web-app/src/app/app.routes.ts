2b47a1554a5c86762481721776427d7505938bf4
Simon Flohr <sflohr2@wgu.edu>
2025-01-26 12:03:12 -0600

    feat(frontend): create RegisterComponent and update routes
    - Generate `RegisterComponent` for user registration functionality.
    - Update `app.routes.ts` to include a route for the registration page.

--- frontend/angular-bondspace-web-app/src/app/app.routes.ts
+++ frontend/angular-bondspace-web-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent }
 ];
 
 export const AppRoutingModule = RouterModule.forRoot(routes);
