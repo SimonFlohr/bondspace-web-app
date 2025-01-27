25e716108a2b6924fd3d8b2a33334662bf3fe044
Simon Flohr <sflohr2@wgu.edu>
2025-01-26 18:40:58 -0600

    feat(frontend): create DashboardComponent and update routes
    - Generate 'DashboardComponent' for peronal dashboard functionality.
    - Update 'app.routes.ts' to include a route for the dashboard page.

--- frontend/angular-bondspace-web-app/src/app/app.routes.ts
+++ frontend/angular-bondspace-web-app/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: 'dashboard', component: DashboardComponent }
 ];
 
 export const AppRoutingModule = RouterModule.forRoot(routes);
