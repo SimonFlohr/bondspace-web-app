005882e435123c18cbe0dff47b1aeb1dbdf9e46a
Simon Flohr <sflohr2@wgu.edu>
2025-01-27 16:02:48 -0600

    feat(frontend): implement dashboard UI and functionality
    - Added responsive HTML layout to `dashboard.component.html`, including a personalized welcome message, placeholder profile picture, and "Create new space" button.
    - Integrated dynamic content rendering with Angular bindings, such as `{{ userName }}`.
    - Styled the dashboard in `dashboard.component.css`, adding hover effects and a clean design for interactive elements.
    
    These changes lay the foundation for a fully functional dashboard for users to access spaces and create new ones.

--- frontend/angular-bondspace-web-app/src/app/component/dashboard/dashboard.component.ts
+++ frontend/angular-bondspace-web-app/src/app/component/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,6 +11,24 @@ import { RouterModule } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  private authSubscription?: Subscription;
+  userName = '';
+  userUserSpaces = [];
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+      this.authSubscription = this.authService.isAuthenticated().subscribe({
+        next: (auth) => {
+          if (auth) {
+            this.authService.getCurrentUser().subscribe(user => {
+              this.userName = user.firstName;
+            });
+          }
+        }
+      });
+  }
 
 }
