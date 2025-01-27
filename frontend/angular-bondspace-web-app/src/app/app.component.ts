6ec7c343f9e6019783dc4b5bad1f348018db2408
Simon Flohr <sflohr2@wgu.edu>
2025-01-26 23:24:04 -0600

    feat(frontend): generate FooterComponent and include it in app.component.html
    - Generated FooterComponent and included it in app.component.html.
    - Refactored the imports section in app.component.ts for better legibility.

--- frontend/angular-bondspace-web-app/src/app/app.component.ts
+++ frontend/angular-bondspace-web-app/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./component/navbar/navbar.component";
+import { FooterComponent } from "./component/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
