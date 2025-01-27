9e6c47d18314aa15d04d10745448388f3472483c
Simon Flohr <sflohr2@wgu.edu>
2025-01-26 19:31:35 -0600

    feat(frontend): add default route redirection based on auth status
    - Add default route redirect to login page.
    - Utilize existing auth guards for handling authenticated users.

--- frontend/angular-bondspace-web-app/src/app/app.routes.ts
+++ frontend/angular-bondspace-web-app/src/app/app.routes.ts
@@ -1,11 +1,20 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { AuthGuard } from './guard/auth-guard.service';
 import { UnauthGuard } from './guard/unauth-guard.service';
+import { inject } from '@angular/core';
+import { AuthService } from './service/auth.service';
+import { map, take } from 'rxjs/operators';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
     {
         path: 'dashboard',
         component: DashboardComponent,
